9751f797147f78564e56a2bf0f4db6b74b0613d9
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-06-02 11:54:52 +0300

    pain with stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 import { ElementStates } from "../../types/element-states";
 import { stringCharsProps } from "../../types/types";
@@ -9,22 +9,75 @@ import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from "./stack-page.module.css";
-import { Stack } from "./utils";
+import { IStack, Stack } from "./utils";
 
 export const StackPage: React.FC = () => {
+  const exampleStack = new Stack<string>();
+
   const [inputValue, setInputValue] = useState<string>("");
   const [arrayOfLetters, setArrayOfLetters] = useState<stringCharsProps[]>([]);
   const [pushing, setPushing] = useState(false);
   const [popping, setPopping] = useState(false);
+  const [stack, setStack] = useState<IStack<string>>(exampleStack);
 
-  const StackMethods = new Stack(
-    setInputValue,
-    setPopping,
-    setPushing,
-    setArrayOfLetters,
-    arrayOfLetters,
-    inputValue
-  );
+  const sortAndWait = async (arr: stringCharsProps[]) => {
+    setArrayOfLetters([...arr]);
+    await delay(SHORT_DELAY_IN_MS);
+  };
+
+  const pushAndRender = async () => {
+    // Лочим кнопки
+    setPushing(true);
+    setInputValue("");
+    // Пушим элемент и сохраняем измененный стек в стейт
+    stack.push(inputValue);
+    // Сбрасываем все кружки из стейта
+    arrayOfLetters.forEach((el) => {
+      el.state = ElementStates.Default;
+      el.head = "";
+    });
+    // Достаем последний элемент из стека и пушим его в стейт
+    const newElement = stack!.peak();
+    arrayOfLetters.push({
+      char: newElement ? newElement : "",
+      state: ElementStates.Default,
+    });
+    await sortAndWait(arrayOfLetters);
+    // Даём голове отметку
+    arrayOfLetters[arrayOfLetters.length-1].head = "top";
+    arrayOfLetters[arrayOfLetters.length-1].state = ElementStates.Changing;
+    await sortAndWait(arrayOfLetters);
+    // Анлочим кнопки
+    setPushing(false);
+    console.log(arrayOfLetters);
+  };
+
+  const popAndRender = async () => {
+    // Лочим кнопки
+    setPopping(true);
+    // Удаляем элемент из стека
+    stack!.pop();
+    // Теперь пикáем, если элементов не осталось - рендерим пустой массив
+    const size = stack.getSize();
+    console.log(size);
+    if (size !== 0) {
+      arrayOfLetters.pop();
+      setArrayOfLetters([...arrayOfLetters]);
+      await delay(SHORT_DELAY_IN_MS);
+      arrayOfLetters[arrayOfLetters.length - 1].state = ElementStates.Changing;
+      arrayOfLetters[arrayOfLetters.length - 1].head = "top";
+      setArrayOfLetters([...arrayOfLetters]);
+    } else {
+      setArrayOfLetters([]);
+    }
+    // Анлочим кнопки
+    setPopping(false);
+  };
+
+  const clear = async () => {
+    stack.clear();
+    setArrayOfLetters([]);
+  };
 
   return (
     <SolutionLayout title="Стек">
@@ -44,25 +97,21 @@ export const StackPage: React.FC = () => {
           isLoader={pushing}
           text="Добавить"
           type="button"
-          onClick={() =>
-            StackMethods.push()
-          }
+          onClick={() => pushAndRender()}
         />
         <Button
           isLoader={popping}
           disabled={!arrayOfLetters.length || pushing}
           text="Удалить"
           type="button"
-          onClick={() =>
-            StackMethods.pop()
-          }
+          onClick={() => popAndRender()}
         />
         <Button
           extraClass={styles.resetButton}
           disabled={!arrayOfLetters.length || pushing || popping}
           text="Очистить"
           type="button"
-          onClick={() => StackMethods.setArrayOfLetters([])}
+          onClick={() => clear()}
         />
       </InputContainer>
       <ul className={styles.circleList}>
--- src/components/stack-page/utils.tsx
+++ src/components/stack-page/utils.tsx
@@ -3,7 +3,38 @@ import { ElementStates } from "../../types/element-states";
 import { stringCharsProps } from "../../types/types";
 import { delay } from "../../utils/utils";
 
-export class Stack {
+export interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+  getSize: () => number;
+  clear: () => void
+}
+
+export class Stack<T> implements IStack<T> {
+  container: T[] = [];
+
+  push = (item: T): void => {
+    this.container.push(item);
+  };
+
+  pop = (): void => {
+    this.container.pop();
+  };
+
+  peak = (): T | null => {
+    let size = this.getSize();
+    if (size) return this.container[size - 1];
+    else return null;
+  };
+
+  getSize = () => this.container.length;
+
+  clear = () => this.container.length = 0;
+}
+
+/*
+export class Stack<T> {
   setInputValue: React.Dispatch<React.SetStateAction<string>>;
   setPopping: React.Dispatch<React.SetStateAction<boolean>>;
   setPushing: React.Dispatch<React.SetStateAction<boolean>>;
