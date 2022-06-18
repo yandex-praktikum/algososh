34ee44fd1f6677ac085fb5da536f66b3b56ad93c
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-06-18 14:32:11 +0300

    circle done

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -1,23 +1,29 @@
 import { Button } from "./button";
 import React from "react";
 import renderer from "react-test-renderer";
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 
-it("Кнопка с текстом рендерится без ошибок", () => {
-  const tree = renderer.create(<Button text="Sample text" />).toJSON();
-  expect(tree).toMatchSnapshot();
+
+describe("Проверка рендера кнопки", () => {
+  it("Кнопка с текстом рендерится без ошибок", () => {
+    const tree = renderer.create(<Button text="Sample text" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Кнопка без текста рендерится без ошибок", () => {
+    const tree = renderer.create(<Button />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Заблокированная кнопка рендерится без ошибок", () => {
+    const tree = renderer.create(<Button disabled={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Кнопка в состоянии загрузки рендерится без ошибок", () => {
+    const tree = renderer.create(<Button isLoader={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
-it("Кнопка без текста рендерится без ошибок", () => {
-  const tree = renderer.create(<Button />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
 
-it("Заблокированная кнопка рендерится без ошибок", () => {
-  const tree = renderer.create(<Button disabled={true} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-it("Кнопка в состоянии загрузки рендерится без ошибок", () => {
-  const tree = renderer.create(<Button isLoader={true} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
