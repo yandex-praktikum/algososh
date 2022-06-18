daa77cd27facb75bb0e51aef829ec22edbba448f
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-06-18 11:35:46 +0300

    Package inconsistency and button tests

---
+++ src/components/ui/button/button.test.js
@@ -0,0 +1,23 @@
+import { Button } from "./button";
+import React from "react";
+import renderer from "react-test-renderer";
+
+it("Кнопка с текстом рендерится без ошибок", () => {
+  const tree = renderer.create(<Button text="Sample text" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("Кнопка без текста рендерится без ошибок", () => {
+  const tree = renderer.create(<Button />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("Заблокированная кнопка рендерится без ошибок", () => {
+  const tree = renderer.create(<Button disabled={true} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("Кнопка в состоянии загрузки рендерится без ошибок", () => {
+  const tree = renderer.create(<Button isLoader={true} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
