8f8f23969d37a79c17e84556eab22bf9988dea3a
RomanMitaki <r.mitaki@yandex.ru>
2023-02-28 21:53:56 +0300

    add List-page initial list rendering

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import styles from "./list-page.module.css";
 import {Input} from "../ui/input/input";
@@ -6,8 +6,31 @@ import {Button} from "../ui/button/button";
 import {nanoid} from "nanoid";
 import {Circle} from "../ui/circle/circle";
 import {defineCircleState} from "../queue-page/utils/defineCircleState";
+import {useForm} from "../../services/hooks/useForm";
+import {Queue} from "../queue-page/types/types";
+import {ArrowIcon} from "../ui/icons/arrow-icon";
+import {LinkedList} from "./types/types";
+
 
 export const ListPage: React.FC = () => {
+    //const {values, handleChange, setValues} = useForm({});
+    const [circlesList, setList] = useState<Array<string> | null>(null);
+    /*const [queue, setQueue] = useState<Queue<string> | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean | undefined>(false);
+    const [index, setIndex] = useState<number>(-Infinity);
+    const [isEnqueue, setIsEnqueue] = useState<boolean | undefined>(false);
+    const [isDequeue, setIsDequeue] = useState<boolean | undefined>(false);
+    const [isClear, setIsClear] = useState<boolean | undefined>(false);
+    const [edges, setEdges] = useState<number[]>([-Infinity, -Infinity]);*/
+
+    let array: string[] = ['0', '34', '8', '1'];
+    let initialList = new LinkedList(array);
+    let initialCirclesList = initialList.toArray();
+
+    useEffect(() => {
+        setList(initialCirclesList);
+    }, []);
+
     return (
         <SolutionLayout title="Связный список">
             <div className={styles.formContainer}>
@@ -84,20 +107,29 @@ export const ListPage: React.FC = () => {
             </div>
 
 
-            {/*<ul className={styles.circleList}>
-          {circlesList?.map((char, idx) => {
-            return (
-                <li key={nanoid()} className={styles.listItem}>
-                  <Circle letter={`${char}`}
-                          index={idx}
-                          state={defineCircleState(index, idx)}
-                          head={idx === edges[0] ? 'head' : ''}
-                          tail={idx === edges[1] ? 'tail' : ''}
-                  />
-                </li>
-            )
-          })}
-        </ul>*/}
+            <ul className={styles.circleList}>
+                {circlesList?.map((char, idx) => {
+                    return (
+                        <li key={nanoid()} className={styles.listItem}>
+                            <Circle letter={`${char}`}
+                                    extraClass={`${styles.circle}`}
+                                //index={idx}
+                                //state={defineCircleState(index, idx)}
+                                //head={idx === edges[0] ? 'head' : ''}
+                                //tail={idx === edges[1] ? 'tail' : ''}
+                            />
+                            {/*<Circle letter={`${char}`}
+                                    index={idx}
+                                //state={defineCircleState(index, idx)}
+                                //head={idx === edges[0] ? 'head' : ''}
+                                //tail={idx === edges[1] ? 'tail' : ''}
+                            />*/}
+                            {idx < circlesList.length - 1 &&
+                                <ArrowIcon/>}
+                        </li>
+                    )
+                })}
+            </ul>
         </SolutionLayout>
     );
 };
