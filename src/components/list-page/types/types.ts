09d03d4be0ca2ae3e45dbaa74b0e6cb4ef3163af
RomanMitaki <r.mitaki@yandex.ru>
2023-02-28 21:19:06 +0300

    add List-page class LinkedList (except deleteByIndex method)

---
+++ src/components/list-page/types/types.ts
@@ -0,0 +1,161 @@
+class Node<T> {
+    value: T;
+    next: Node<T> | null;
+
+    constructor(value: T, next?: Node<T> | null) {
+        this.value = value;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+interface ILinkedList<T> {
+    prepend: (element: T) => void;
+    append: (element: T) => void;
+    deleteHead: () => void;
+    deleteTail: () => void;
+    addByIndex: (element: T, index: number) => void;
+    //deleteByIndex: () => number;
+    toArray: () => T[] | null;
+}
+
+export class LinkedList<T> implements ILinkedList<T> {
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
+    private size: number;
+
+    constructor(array?: T[]) {
+        this.head = null;
+        this.size = 0;
+        this.tail = null;
+        array?.forEach(element => this.append(element));
+    }
+
+
+    prepend(element: T) {
+        const node = new Node(element);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    deleteHead() {
+        if (this.size === 1) {
+            this.head = null;
+            this.size = 0;
+            this.tail = null;
+        } else {
+            this.head = this.head!.next;
+            this.size--;
+        }
+    }
+
+    append(element: T) {
+        const node = new Node(element);
+        if (!this.tail) {
+            this.tail = node;
+            this.head = node;
+        } else if (this.size === 1) {
+            this.head!.next = node;
+            this.tail = node;
+        } else {
+            let n = this.head!.next;
+            while (this.size > 1 && n?.next) {
+                n = n.next;
+            }
+            n!.next = node;
+            this.tail = node;
+        }
+        this.size++;
+    }
+
+    deleteTail() {
+        if (this.size === 1) {
+            this.head = null;
+            this.size = 0;
+            this.tail = null;
+        } else {
+            let n = this.head;
+            let prev = this.head;
+            while (this.size > 1 && n?.next) {
+                n = n.next;
+                prev = prev!.next;
+            }
+            prev!.next = null;
+            this.tail = prev;
+            this.size--;
+        }
+    }
+
+    addByIndex(element: T, index: number) {
+        if (index < 0 || index > this.size) {
+            console.log("Enter a valid index");
+            return;
+        } else {
+            const node = new Node(element);
+            // добавить элемент в начало списка
+            if (index === 0) {
+                node.next = this.head;
+                this.head = node;
+            } else {
+                let curr = this.head;
+                // перебрать элементы в списке до нужной позиции
+                while (index > 1 && curr) {
+                    curr = curr.next;
+                    index--;
+                }
+                // добавить элемент
+                if (curr) {
+                    let next = curr.next;
+                    curr.next = node;
+                    if (next) {
+                        node.next = next;
+                    } else {
+                        this.tail = curr.next;
+                    }
+                }
+            }
+            this.size++;
+        }
+    }
+
+    /*  deleteByIndex(index: number, element: T) {
+          if (index < 0 || index > this.size) {
+              console.log("Enter a valid index");
+              return;
+          }
+          let dummyHead = new Node(element); // добавим в начало пустой узел
+          dummyHead.next = this.head;
+          let curr = dummyHead;
+          let prev = curr;
+          let n = 0;
+          while (n <= index) {
+              curr = curr.next as Node<T>;
+              prev = prev.next as Node<T>;
+              n++;
+          }
+          prev.next = curr.next;
+          curr.next = null;
+          this.size--;
+      }*/
+
+    /*getSize() {
+        return this.size;
+    }*/
+
+    toArray(): T[] | null {
+        let curr = this.head;
+        let res = [];
+        while (curr) {
+            res.push(curr.value);
+            curr = curr.next;
+        }
+        return res;
+    }
+
+
+}
