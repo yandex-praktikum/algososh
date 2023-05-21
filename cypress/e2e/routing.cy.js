d0a0c64ea8264fe9a5e43fb34a3661a71b66eb9b
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-05-22 01:23:02 +0300

    Рефакторинг некоторых тестов

--- cypress/e2e/routing.cy.js
+++ cypress/e2e/routing.cy.js
@@ -5,38 +5,38 @@ describe('reverse string function works correctly', () => {
 
   it('shows correct page after recursion string item click', () => {
     cy.get('a[href="/recursion"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/recursion')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/recursion')
+    cy.get('form')
   })
 
   it('shows correct page after fibonacci item click', () => {
     cy.get('a[href="/fibonacci"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/fibonacci')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/fibonacci')
+    cy.get('form')
   })
 
   it('shows correct page after sorting item click', () => {
     cy.get('a[href="/sorting"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/sorting')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/sorting')
+    cy.get('form')
   })
 
   it('shows correct page after stack item click', () => {
     cy.get('a[href="/stack"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/stack')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/stack')
+    cy.get('form')
   })
 
   it('shows correct page after queue item click', () => {
     cy.get('a[href="/queue"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/queue')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/queue')
+    cy.get('form')
   })
 
   it('shows correct page after list item click', () => {
     cy.get('a[href="/list"]').click()
-    cy.location('pathname', {timeout: 60000}).should('include', '/list')
-    cy.get('form').should('have.length', 1)
+    cy.location('pathname').should('include', '/list')
+    cy.get('form')
   })
 
 })
--- cypress/e2e/string.cy.js
+++ cypress/e2e/string.cy.js
@@ -1,4 +1,6 @@
+const DELAY = 1000;
 const checkClass = (classes, value) => {expect(classes).to.contain(value)}
+const SAMPLE_TEXT = 'hello'
 
 describe('reverse string function works correctly', () => {
   beforeEach(() => {
@@ -10,7 +12,7 @@ describe('reverse string function works correctly', () => {
   })
 
   it('should be animate reversing string correctly', () => {
-    cy.get('form input[type="text"]').type('hello')
+    cy.get('form input[type="text"]').type(SAMPLE_TEXT)
     cy.get('form button[type="submit"]').click()
 
     cy.get('form').next().children('div').as('circles')
@@ -19,51 +21,51 @@ describe('reverse string function works correctly', () => {
     cy.get('@circles').should('have.length', 5)
 
     // Проверяем первый и последний элементы
-    cy.get('@circles').first().as('1').should('have.attr', 'class')
+    cy.get('@circles').first().as('0').should('have.attr', 'class')
       .should(($class) => {
         checkClass($class, 'current')
       })
-    cy.get('@circles').last().as('5').should('have.attr', 'class')
+    cy.get('@circles').last().as('4').should('have.attr', 'class')
       .should(($class) => {
         checkClass($class, 'current')
       })
 
-    cy.get('@1').find('p[class*="letter"]').as('letter1').should('have.text', 'h')
-    cy.get('@5').find('p[class*="letter"]').as('letter5').should('have.text', 'o')
-    cy.wait(1000)
+    cy.get('@0').find('p[class*="letter"]').as('letter0').should('have.text', SAMPLE_TEXT[0])
+    cy.get('@4').find('p[class*="letter"]').as('letter4').should('have.text', SAMPLE_TEXT[4])
+    cy.wait(DELAY)
 
     // Первый и последний элементы отсортированы, следующие должны подсвечиваться текущими
-    cy.get('@1').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@5').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@circles').eq(1).as('2').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'current')
-      })
-    cy.get('@circles').eq(3).as('4').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'current')
-      })
-
-    cy.get('@letter1').should('have.text', 'o')
-    cy.get('@letter5').should('have.text', 'h')
-    cy.get('@2').find('p[class*="letter"]').as('letter2').should('have.text', 'e')
-    cy.get('@4').find('p[class*="letter"]').as('letter4').should('have.text', 'l')
-
-    cy.wait(1000)
-
-    // Второй и четвертый элементы отсортированы
-    // Поскольку строка содержит нечетное количество символов, символ посередине сразу становится отсортированным
-    cy.get('@2').should('have.attr', 'class')
+    cy.get('@0').should('have.attr', 'class')
       .should(($class) => {
         checkClass($class, 'sorted')
       })
     cy.get('@4').should('have.attr', 'class')
+      .should(($class) => {
+        checkClass($class, 'sorted')
+      })
+    cy.get('@circles').eq(1).as('1').should('have.attr', 'class')
+      .should(($class) => {
+        checkClass($class, 'current')
+      })
+    cy.get('@circles').eq(3).as('3').should('have.attr', 'class')
+      .should(($class) => {
+        checkClass($class, 'current')
+      })
+
+    cy.get('@letter0').should('have.text', 'o')
+    cy.get('@letter4').should('have.text', 'h')
+    cy.get('@1').find('p[class*="letter"]').as('letter1').should('have.text', SAMPLE_TEXT[1])
+    cy.get('@3').find('p[class*="letter"]').as('letter3').should('have.text', SAMPLE_TEXT[3])
+
+    cy.wait(DELAY)
+
+    // Второй и четвертый элементы отсортированы
+    // Поскольку строка содержит нечетное количество символов, символ посередине сразу становится отсортированным
+    cy.get('@1').should('have.attr', 'class')
+      .should(($class) => {
+        checkClass($class, 'sorted')
+      })
+    cy.get('@3').should('have.attr', 'class')
       .should(($class) => {
         checkClass($class, 'sorted')
       })
@@ -72,8 +74,8 @@ describe('reverse string function works correctly', () => {
         checkClass($class, 'sorted')
       })
 
-    cy.get('@letter2').should('have.text', 'l')
-    cy.get('@letter4').should('have.text', 'e')
+    cy.get('@letter1').should('have.text', SAMPLE_TEXT[3])
+    cy.get('@letter3').should('have.text', SAMPLE_TEXT[1])
 
   })
 
