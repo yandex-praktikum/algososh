55ef6cda37e854c6070ccab6f40a2eb961d0fa36
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-05-26 16:02:14 +0300

    Рефакторинг для e2e тестов для задачи с разворотом строки

---
+++ src/components/string/string.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import renderer from 'react-test-renderer'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+import { StringComponent } from './string'
+
+it('Корректно разворачивается строка с четным количеством символов', () => {
+  const tree = renderer
+    .create(<StringComponent text='Кнопка' />)
+    .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+it('Кнопка без текста рендерится без ошибок', () => {
+  const tree = renderer
+    .create(<Button />)
+    .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+it('Заблокированная кнопка рендерится без ошибок', () => {
+  const tree = renderer
+    .create(<Button disabled={true} text='Кнопка'/>)
+    .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+it('Кнопка с лоадером рендерится без ошибок', () => {
+  const tree = renderer
+    .create(<Button isLoader={true}/>)
+    .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+it('Обработчик клика по кнопке рендерится без ошибок', () => {
+  const tree = renderer
+    .create(<Button text='Нажми' onClick={() => alert('Click!')}/>)
+    .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+it('Обработчик клика по кнопке вызывает корректный alert', () => {
+  window.alert = jest.fn()
+
+  render(<Button text='Нажми' onClick={() => alert('Click!')}/>)
+
+  fireEvent.click(screen.getByText('Нажми'))
+
+  expect(window.alert).toHaveBeenCalledWith('Click!')
+})
