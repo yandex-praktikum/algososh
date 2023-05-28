d9bf318e38c9b532ad7c2ea4017b6944ac726134
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-05-28 23:02:01 +0300

    Рефакторинг e2e теста для переворота строки; добавлен e2e тест для последовательности Фибоначчи

---
+++ cypress/e2e/fibonacci.cy.js
@@ -0,0 +1,39 @@
+import { fibonacci } from '../../src/utils/fibonacci'
+
+const DELAY = 500
+const checkClass = (classes, value) => {expect(classes).to.contain(value)}
+const N = 10
+
+describe('reverse string function works correctly', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/fibonacci')
+  })
+
+  it('should be disabled button when there is no number to count', () => {
+    cy.contains('Рассчитать').should('have.attr', 'disabled')
+  })
+
+  it('should be animate couning correct fibonscci number', () => {
+    cy.get('form input[type="number"]').type(N)
+    cy.get('form button[type="submit"]').click()
+
+    cy.get('form').next().as('container')
+
+    for (let i = 0; i < N + 1; i++) {
+      cy.get('@container').children('div').eq(i).find('p[class*="letter"]').then(($p) => {
+        const n = $p.text()
+
+        if (i == 0 || i == 1) {
+          expect(n, 'text').to.equal('1')
+        } else {
+          expect(n, 'text').to.equal(String(fibonacci(i)))
+        }
+      })
+
+    }
+    cy.wait(DELAY)
+
+
+  })
+
+})
--- cypress/e2e/string.cy.js
+++ cypress/e2e/string.cy.js
@@ -1,6 +1,7 @@
-const DELAY = 1000;
+const DELAY = 1000
 const checkClass = (classes, value) => {expect(classes).to.contain(value)}
 const SAMPLE_TEXT = 'hello'
+const SAMPLE_TEXT_LENGTH = SAMPLE_TEXT.length
 
 describe('reverse string function works correctly', () => {
   beforeEach(() => {
@@ -18,64 +19,45 @@ describe('reverse string function works correctly', () => {
     cy.get('form').next().children('div').as('circles')
 
     // Количество символов в строке
-    cy.get('@circles').should('have.length', 5)
+    cy.get('@circles').should('have.length', SAMPLE_TEXT_LENGTH)
 
-    // Проверяем первый и последний элементы
-    cy.get('@circles').first().as('0').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'current')
-      })
-    cy.get('@circles').last().as('4').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'current')
-      })
-
-    cy.get('@0').find('p[class*="letter"]').as('letter0').should('have.text', SAMPLE_TEXT[0])
-    cy.get('@4').find('p[class*="letter"]').as('letter4').should('have.text', SAMPLE_TEXT[4])
-    cy.wait(DELAY)
-
-    // Первый и последний элементы отсортированы, следующие должны подсвечиваться текущими
-    cy.get('@0').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@4').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@circles').eq(1).as('1').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'current')
-      })
-    cy.get('@circles').eq(3).as('3').should('have.attr', 'class')
+    for (let i = 0; i < Math.floor(SAMPLE_TEXT_LENGTH / 2); i++) {
+      cy.get('@circles').eq(i).should('have.attr', 'class')
+        .should(($class) => {
+          checkClass($class, 'current')
+        })
+      cy.get('@circles').eq(SAMPLE_TEXT_LENGTH - i - 1).should('have.attr', 'class')
       .should(($class) => {
         checkClass($class, 'current')
       })
 
-    cy.get('@letter0').should('have.text', 'o')
-    cy.get('@letter4').should('have.text', 'h')
-    cy.get('@1').find('p[class*="letter"]').as('letter1').should('have.text', SAMPLE_TEXT[1])
-    cy.get('@3').find('p[class*="letter"]').as('letter3').should('have.text', SAMPLE_TEXT[3])
+      if (i - 1 >= 0) {
+        cy.get('@circles').eq(i - 1).should('have.attr', 'class')
+          .should(($class) => {
+            checkClass($class, 'sorted')
+          })
+        cy.get('@circles').eq(SAMPLE_TEXT_LENGTH - i).should('have.attr', 'class')
+          .should(($class) => {
+            checkClass($class, 'sorted')
+          })
 
-    cy.wait(DELAY)
+        cy.get('@circles').eq(i - 1).find('p[class*="letter"]').as('letter1').should('have.text', SAMPLE_TEXT[SAMPLE_TEXT_LENGTH - i])
+        cy.get('@circles').eq(SAMPLE_TEXT_LENGTH - i).find('p[class*="letter"]').as('letter3').should('have.text', SAMPLE_TEXT[i - 1])
+      }
 
-    // Второй и четвертый элементы отсортированы
-    // Поскольку строка содержит нечетное количество символов, символ посередине сразу становится отсортированным
-    cy.get('@1').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@3').should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
-    cy.get('@circles').eq(2).should('have.attr', 'class')
-      .should(($class) => {
-        checkClass($class, 'sorted')
-      })
+      cy.get('@circles').eq(i).find('p[class*="letter"]').should('have.text', SAMPLE_TEXT[i])
+      cy.get('@circles').eq(SAMPLE_TEXT_LENGTH - i - 1).find('p[class*="letter"]').should('have.text', SAMPLE_TEXT[SAMPLE_TEXT_LENGTH - i - 1])
 
-    cy.get('@letter1').should('have.text', SAMPLE_TEXT[3])
-    cy.get('@letter3').should('have.text', SAMPLE_TEXT[1])
+      cy.wait(DELAY)
+
+
+    }
+    if (SAMPLE_TEXT_LENGTH % 2) {
+      cy.get('@circles').eq(Math.floor(SAMPLE_TEXT_LENGTH / 2)).should('have.attr', 'class')
+        .should(($class) => {
+          checkClass($class, 'sorted')
+        })
+    }
 
   })
 
